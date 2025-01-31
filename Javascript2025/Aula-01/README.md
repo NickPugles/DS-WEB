9a4e042d4f0018798512352a923a82d4ac65e703
NickPugles <nicollizatorre2007@gmail.com>
2025-01-31 12:30:06 -0300

    Aula01-JS

---
+++ Javascript2025/Aula-01/README.md
@@ -0,0 +1,10 @@
+# DS-WEB
+
+- LET: quando a função da variável só é permitida no bloco que ela está (local);
+- VAR: é de utilização GLOBAL, portanto permiti a redeclaração e reatribuição;
+- CONST (constantes): O modo de declaração é LOCAL respeitando os blocos e não permitindo reatribuição nem redeclaração;
+- Variáveis: Os tipos mais usados são os numéricos e de texto;
+- toFixed(): adiciona casas decimais que decidir a sua variável;
+- toLocaleString: transforma o valor digitado na moeda do país selecionado, fazendo automaticamente a conversão e já adaptando, se necessário.
+-->
+"# DS-WEB" 
---
+++ Javascript2025/Aula-01/script.js
@@ -0,0 +1,26 @@
+    console.log("Olá Mundo"); //exibi no log(em cima da página)
+
+//Exemplo de Alert
+    alert("Olá mundo 1"); //um aviso que aparece na tela ao abrir
+
+//Exemplo de Confirm
+    var teste = confirm("Olá mundo 2");
+    //exibindo a opção escolhida pelo usuário como true ou false
+    console.log(teste)
+
+//Exemplo de Prompt    serve para digitar um texto
+    var testePromt = prompt("Digite seu nome"); //recebe informação vinda do usuário. Se trata de um espaço para o usuário escrever
+    //exibindo o nome do usuário
+    console.log(testePromt);
+    alert(testePromt);
+
+//Tentativa de Adição
+      //declaro = atribui valor
+    var numero1 = Number.parseInt(prompt("Digite seu primeiro número: "));
+    var numero2 = Number.parseInt(prompt("Digite seu segundo número: "));
+    //Verificando o tipo de variavel
+    console.log(typeof(numero1));
+    alert(numero1 + numero2);
+
+
+
