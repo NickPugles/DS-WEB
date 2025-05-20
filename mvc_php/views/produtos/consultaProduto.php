2ba1ec52cc1f9b26eeb4d4b87cbae5e1795dbae0
NickPugles <nicollizatorre2007@gmail.com>
2025-05-20 15:58:05 -0300

    AtivAvaliativa_PhP

---
+++ mvc_php/views/produtos/consultaProduto.php
@@ -0,0 +1,21 @@
+<center><h1>Produto cadastrado</h1></center>
+
+<table class="produtos">
+    <tr>
+        <th>Nome:</th>
+        <th>Preço:</th>
+        <th>Estoque:</th>
+    </tr>
+    <?php
+        foreach($dados as $dado){
+            extract($dado);
+            echo "<tr>";
+                echo "<td>$nomeProduto</td>";
+                echo "<td>$precoProduto</td>";
+                echo "<td>$estoqueProduto</td>";
+            echo "</tr>";
+        }
+    ?>
+</table>
+<br>
+<center><a href="/mvc_php/produto"><button class='botao1'><i class="fa-solid fa-backward"></i></button></a></center>
